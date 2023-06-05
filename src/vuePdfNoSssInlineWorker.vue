33d44e33593785658b6a3b079b30397b7813a4cf
Guillermo Simmross <gsimmross@gmail.com>
2023-06-05 11:50:27 +0200

    Update vuePdfNoSssInlineWorker.vue

--- src/vuePdfNoSssInlineWorker.vue
+++ src/vuePdfNoSssInlineWorker.vue
@@ -3,7 +3,7 @@
 	import componentFactory from './componentFactory.js'
 
 	const pdfjsWrapper = require('./pdfjsWrapper.js').default;
-	const PDFJS = require('pdfjs-dist/legacy/build/pdf.min.js');
+	const PDFJS = require('pdfjs-dist/build/pdf.min.js');
 
 	if (typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1) {
 		PDFJS.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.5.207/pdf.worker.min.js';
