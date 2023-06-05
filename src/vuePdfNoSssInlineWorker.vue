d4b8b9ea4175ae1e73b97b4d7b59011a82edca20
Guillermo Simmross <gsimmross@gmail.com>
2023-06-05 11:03:34 +0200

    Create vuePdfNoSssInlineWorker.vue

---
+++ src/vuePdfNoSssInlineWorker.vue
@@ -0,0 +1,15 @@
+<style src="./annotationLayer.css"></style>
+<script>
+	import componentFactory from './componentFactory.js'
+
+	const pdfjsWrapper = require('./pdfjsWrapper.js').default;
+	const PDFJS = require('pdfjs-dist/legacy/build/pdf.min.js');
+
+	if (typeof window !== 'undefined' && 'Worker' in window && navigator.appVersion.indexOf('MSIE 10') === -1) {
+		PDFJS.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.5.207/pdf.worker.min.js';
+	}
+
+	var component = componentFactory(pdfjsWrapper(PDFJS));
+
+	export default component;
+</script>
